ba826e62dd8bcaca041145b2cd425af46088cd02
1emniscata <aleksandrsuzen@gmail.com>
2021-06-24 20:29:14 +0300

    aaa

--- UITAP/tests/conftest.py
+++ UITAP/tests/conftest.py
@@ -5,17 +5,7 @@ import pytest
 
 @pytest.fixture
 def browser():
-  # capabilities = {
-  #   "browserName": "UNKNOWN",
-  #   "browserVersion": "",
-  #   "selenoid:options": {
-  #     "enableVNC": True,
-  #     "enableVideo": False
-  #   }
-  # }
-  # driver = webdriver.Remote(
-  #   command_executor="http://127.0.0.1:4444//wd/hub",
-  #   desired_capabilities=capabilities)
+
   driver = Chrome()
   driver.implicitly_wait(10)
   yield driver
